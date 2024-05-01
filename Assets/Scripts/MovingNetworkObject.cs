090c6451d51798679206124acfe37fbcbf77c482
Aaron Zhang <ajr.zhang@mail.utoronto.ca>
2024-05-01 00:01:43 -0400

    add custom interpolation for players

---
+++ Assets/Scripts/MovingNetworkObject.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class MovingNetworkObject : NetworkBehaviour
+{
+    [SerializeField] private float positionErrorThreshold;
+    [SerializeField] private bool isExtrapolationEnabled;
+    [SerializeField] private bool isInterpolationEnabled;
+    [SerializeField] private int lerpSpeed;
+
+
+    private NetworkVariable<Vector3> networkPosition = new NetworkVariable<Vector3>(new Vector3(0, 0, 0), NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    private NetworkVariable<Vector3> networkVelocity = new NetworkVariable<Vector3>(new Vector3(0, 0, 0), NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    private NetworkVariable<double> networkTime = new NetworkVariable<double>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsOwner)
+        {
+            networkPosition.Value = transform.position;
+            networkVelocity.Value = rb.velocity;
+            networkTime.Value = NetworkManager.Singleton.ServerTime.Time;
+        }
+        else
+        {
+            if (isExtrapolationEnabled)
+            {
+                Vector3 estimatedPosition = networkPosition.Value + networkVelocity.Value * (float)(NetworkManager.Singleton.ServerTime.Time - networkTime.Value);
+
+                Vector3 positionError = estimatedPosition - rb.position;
+                if (positionError.magnitude > positionErrorThreshold)
+                {
+                    rb.position = Vector3.Lerp(rb.position, estimatedPosition, Time.deltaTime * lerpSpeed);
+                }
+
+                // rb.transform.forward = Vector3.Lerp(rb.transform.forward, networkVelocity.Value.normalized, Time.deltaTime * lerpSpeed);
+
+                //transform.position = networkPosition.Value;
+                rb.velocity = networkVelocity.Value;
+            }
+            else if (isInterpolationEnabled)
+            {
+                rb.position = Vector3.Lerp(rb.position, networkPosition.Value, Time.deltaTime * lerpSpeed);
+            }
+            else
+            {
+                transform.position = networkPosition.Value;
+                rb.velocity = networkVelocity.Value;
+            }
+        }
+    }
+}
