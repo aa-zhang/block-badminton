13d3f6aa48f737d0b52ff33dea99b9571aff1d19
Aaron Zhang <ajr.zhang@mail.utoronto.ca>
2025-01-31 20:13:42 -0500

    initial commit for rewriting project with urp

--- Assets/Scripts/Game Manager/GameEnvironmentManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameEnvironmentManager : MonoBehaviour
-{
-    public int id;
-    public bool isTraining;
-}
